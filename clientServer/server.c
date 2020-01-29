1ccb4379f698e4794903213beb86e48220b8649c
Aiswarya S <saiswarya@vmware.com>
2020-01-28 19:08:22 -0800

    from VM

--- clientServer/server.c
+++ clientServer/server.c
@@ -8,15 +8,15 @@ int main(){
 
     char msg[256] = "Hello from server";
     int serverSocket;
-    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0); //socket file descriptor
 
     struct sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
-    serverAddress.sin_port = htons(8080);
+    serverAddress.sin_family = AF_INET;             //ipv4
+    serverAddress.sin_addr.s_addr = INADDR_ANY;     //any available protocol
+    serverAddress.sin_port = htons(8080);           //host to network byte addr translation
 
     bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
-    listen(serverSocket, 5);
+    listen(serverSocket, 5);                        //backlog queue size=5(maximum no. of connection it can wait for)
 
     int clientSocket;
     clientSocket = accept(serverSocket, (struct sockaddr*)&serverAddress, (socklen_t*)&serverAddress);
@@ -24,4 +24,5 @@ int main(){
     send(clientSocket, msg, sizeof(msg), 0);
 
     return 0;
+
 }
